3403d686b9a4eebf4ff5bdf7d26c7f63d7f91ae4
MYEONGGYULEE <brstar96@naver.com>
2018-11-09 22:47:46 +0900

    separate 642 csv files
    1. 기존 RandomNumTest.py의 아웃풋 결과물을 642개의 csv로 확장

--- RFTracker(Test)/RandomNumTest.py
+++ RFTracker(Test)/RandomNumTest.py
@@ -6,25 +6,11 @@ import csv
 RandTau = []
 NrCount = 2500 #Randomize를 수행하는 횟수
 NvCount = 642 #뷰포인트 개수
-csvfile = open('RandomizeTest.csv', 'w', newline='')
-writer = csv.writer(csvfile)
-
-#0~8 사이의 랜덤 정수 8개 출력
-#
-# for i in range(0, 3):
-#     for i in range(0, 3):
-#         RandNrTranslation.append(randint(0,8))
-# print(RandNrTranslation)
-#
-# #0.0~360.0 사이의 랜덤 소수점아래 3자리 실수 8개 출력
-# for j in range(0, 8):
-#     RandNrRotation.append(round(uniform(0.0, 360.0), 3))
-# print(RandNrRotation)
 
 #6개 파라미터 randomize(x, y, z, a, b, c)
-#for i in range(0, 2500):
-
 for i in range(0, NvCount):
+    csvfile = open("./dataSets/RandomizeTest"+str(i+1)+".csv", 'w', newline='')
+    writer = csv.writer(csvfile)
     for j in range(0, NrCount):
         for k in range(0, 3):
             RandTau.append(randint(0,8))
