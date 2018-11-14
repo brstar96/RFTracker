6d6b1c8a7d63d7bd041d8751ecf4d8a6d04587aa
LEEMYEONGGYU <brstar96@naver.com>
2018-11-14 22:44:19 +0900

    img reader update

--- DepthImage/Assets/MeshGeneration.cs
+++ DepthImage/Assets/MeshGeneration.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.IO;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 
 public class MeshGeneration : MonoBehaviour
 {
+    
     void Start()
     {
         CreateCube();
@@ -13,17 +15,42 @@ public class MeshGeneration : MonoBehaviour
 
     private void CreateCube()
     {
-        Vector3[] vertices = {
-            new Vector3 (1, 1, 1),
-            new Vector3 (1, 0, 0),
-            new Vector3 (1, 1, 0),
-            new Vector3 (0, 1, 0),
-            new Vector3 (0, 1, 1),
-            new Vector3 (1, 1, 1),
-            new Vector3 (1, 0, 1),
-            new Vector3 (0, 0, 1),
-        };
+        Vector3 []vertices = new Vector3[20];
+
+        for (int i = 0; i < 20; i++) {
+            vertices[i] = new Vector3(0,0,0);
+        }
+
+        Texture2D GrayscaleImg = new Texture2D(512, 512, TextureFormat.RGBA32, false);
+        string PATH = "DepthSampleImgs/31.png";
+        GrayscaleImg = Resources.Load(PATH, typeof(Texture2D)) as Texture2D;
+        byte[] bytes = new byte[512*512];
+        GrayscaleImg.LoadImage(bytes);
         
+        for(int i = 0; i < 512; i++)
+        {
+            Debug.Log(bytes[i]);
+        }
+
+
+        //Stream imageStreamSource = new FileStream("smiley.png", FileMode.Open, FileAccess.Read, FileShare.Read);
+        //PngBitmapDecoder decoder = new PngBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+        //BitmapSource bitmapSource = decoder.Frames[0];
+
+
+
+
+        //Vector3[] vertices = {
+        //    new Vector3 (1, 1, 1),
+        //    new Vector3 (1, 0, 0),
+        //    new Vector3 (1, 1, 0),
+        //    new Vector3 (0, 1, 0),
+        //    new Vector3 (0, 1, 1),
+        //    new Vector3 (1, 1, 1),
+        //    new Vector3 (1, 0, 1),
+        //    new Vector3 (0, 0, 1),
+        //};
+
         int[] triangles = {
             0, 2, 1, //face front
 			0, 3, 2,
