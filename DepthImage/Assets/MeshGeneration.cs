6cfb9492b87557b03bd862feb5efe5f4b39de030
LEEMYEONGGYU <brstar96@naver.com>
2018-11-15 18:28:33 +0900

    1115 unity minor update2
    1. 픽셀값 뽑아내는 코드 업데이트

--- DepthImage/Assets/MeshGeneration.cs
+++ DepthImage/Assets/MeshGeneration.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 public class MeshGeneration : MonoBehaviour
 {
-    string PATH = "D:/DepthTestImgs/328.png";
+    string PATH = "D:/DepthTestImgs/0.png";
 
     void Start()
     {
@@ -25,35 +25,18 @@ public class MeshGeneration : MonoBehaviour
         }
 
         Texture2D GrayscaleImg = new Texture2D(512, 512, TextureFormat.RGBA32, false);
-        //string PATH = "DepthSampleImgs/31.png";
-        //GrayscaleImg = Resources.Load(PATH, typeof(Texture2D)) as Texture2D;
-        //byte[] bytes = new byte[512 * 512];
-        //GrayscaleImg.LoadImage(bytes);
-
-        //for (int i = 0; i < 512; i++)
-        //{
-        //    Debug.Log(bytes[i]);
-        //}
         byte[] bytes = new byte[512 * 512];
         Texture2D testtex = LoadPNG(PATH);
-       
-        testtex.LoadImage(bytes);
-
-        for (int i=0;i<bytes.Length;i++)
+        Color[] pixcels = testtex.GetPixels(0,0,512,512);
+        Debug.Log("length: "+pixcels.Length);
+        for(int i=0;i<pixcels.Length;i++)
         {
-            if (bytes[i]<=0)
+            if(pixcels[i].r > 0)
             {
-                Debug.Log(bytes[i]/255);
+
             }
-            //if (i % 100 == 0)
-            //{
-            //    Debug.Log("i="+i+"pixel value "+bytes[i] / 255);
-
-            //}
-
         }
-        
-        
+
     }
 
     public Texture2D LoadPNG(string filePath)
@@ -66,7 +49,6 @@ public class MeshGeneration : MonoBehaviour
         if (File.Exists(filePath))
         {
             fileData = File.ReadAllBytes(filePath);
-            Debug.Log(fileData[513]);
             tex = new Texture2D(512, 512);
             tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
         }
