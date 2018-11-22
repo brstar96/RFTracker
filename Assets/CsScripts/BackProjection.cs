5f08bbec7da2e5063f2b4efd6aa9e8b7c9a9a5af
ryuck <1154641033@qq.com>
2018-11-22 20:08:53 +0900

    1122_002_recommit

---
+++ Assets/CsScripts/BackProjection.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class BackProjection : MonoBehaviour
+{
+    string PATH = "D:/DepthTestImgs/0.png";
+    
+    void Start()
+    {
+        BackProj();
+    }
+
+    private void BackProj()
+    {
+        GameObject obj = GameObject.Find("empty");
+
+        List<GameObject> SphereList = new List<GameObject>(); //20개의 sphere 오브젝트를 만들기 위한 변수
+        CreateCube();
+    }
+
+    private void CreateCube()
+    {
+        List<GameObject> SphereList = new List<GameObject>();
+        List<Vector3> vertexPositions = new List<Vector3>();
+        List<Vector3> grayScalePixcels = loadGrayScalePNG(PATH); //loading image from 'PATH' and store to 'testtex' Texture2D variable. 
+        GameObject singleSphere;
+        for (int j = 0; j < grayScalePixcels.Count; j++)
+        {
+            singleSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            singleSphere.transform.localScale = new Vector3(0.0009f, 0.0009f, 0.0009f);
+            SphereList.Add(singleSphere);
+        }
+        for (int count = 0; count < grayScalePixcels.Count; count++)
+        {
+            vertexPositions.Add(new Vector3(grayScalePixcels[count].x / 512f, grayScalePixcels[count].y / 512f, grayScalePixcels[count].z / 5.12f));
+        }
+
+        for (int j = 0; j < SphereList.Count; j++)
+        {
+            //SphereList[j].transform.position = new Vector3(vertexPositions[j].x, vertexPositions[j].y, vertexPositions[j].z);
+            SphereList[j].transform.position = new Vector3(vertexPositions[j].x, vertexPositions[j].y, vertexPositions[j].z * 5);
+           // getDisplacement(); //call 'getDisplacement' function for calculate displacement value per 'GT-backprojected vertex pair'. (11.18)
+        }
+
+    }
+
+    public List<Vector3> loadGrayScalePNG(string filePath)
+    {
+        List<Vector3> grayScalePixcels = new List<Vector3>();
+        Color pixcels;
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(512, 512);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+        }
+
+        for (int height = 0; height < 512; height++)
+        {
+            for (int width = 0; width < 512; width++)
+            {
+                pixcels = tex.GetPixel(height, width);
+                if (pixcels.r < 1)
+                {
+                    grayScalePixcels.Add(new Vector3(height, width, pixcels.r));
+                }
+            }
+        }
+
+        return grayScalePixcels;
+
+        Destroy(tex);
+        grayScalePixcels = null;
+        fileData = null;
+    }
+
+    public void getDisplacement()
+    {
+        Vector3 a = new Vector3(2, 2, 2);
+        Vector3 b = new Vector3(1, 5, 9);
+        //Debug.Log(Vector3.Distance(b, a));
+        //Debug.Log(Vector3.Distance(a, b));
+    }
+
+    public float getDisplacement(Vector3 v1, Vector3 v2)
+    {
+        //v1 : Ground truth vertex position from original 'stanfordbunny.obj' file.
+        //v2 : back-projected vertex position from 'BackprojTest' unity scene.
+        //tree will learn the relationship between displacement scalar value & corresponding transformation parameter tau.
+        float tempDisplacement = 0;
+
+        tempDisplacement = Vector3.Distance(v1, v2);
+        return tempDisplacement;
+
+    }
+
+    public void comunicationWithPython()
+    {
+
+    }
+}
+
+
---
+++ Assets/CsScripts/bunny_Scrpt.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class bunny_Scrpt : MonoBehaviour {
+    private Transform m_Transform;
+    public float speed = 1;
+    GameObject GameObjCama;
+    Camera came;
+    string[] files;
+    public Material mat;
+    private RenderTexture rt;
+    private int image_id = 0;
+    private bool SnapFlag=false;
+
+
+    void Start () {
+        GameObjCama = GameObject.Find("snap");
+        came = GameObjCama.GetComponent<Camera>();
+        rt = new RenderTexture(512, 512, 24);  // 24 bit depth
+        came.targetTexture = rt;
+
+
+        Debug.Log("call cam"+came);
+
+
+        
+    }
+
+
+
+
+    /*
+     *카메라와 같은 뷰 방향에서 
+     * H J 키 로 상하 위치 조정  
+     * W A S D 로 객체가 있는 위치에서 앞 뒤 좌 우로 이동
+     * 
+     */
+    void FixedUpdate () {
+        if (Input.GetKey(KeyCode.H))
+        {
+            this.transform.Translate(0, speed * Time.deltaTime*10, 0);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            this.transform.Translate(0, -speed * Time.deltaTime*10, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0, 0, speed * Time.deltaTime*10);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0, 0, -speed * Time.deltaTime*10);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-speed * Time.deltaTime*10,0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(speed * Time.deltaTime*10,0, 0);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            
+            this.transform.Rotate(5*Input.GetAxis("Mouse X"),0,0);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            this.transform.Rotate(0,5 * Input.GetAxis("Mouse X"), 0);
+
+        }
+        if (Input.GetMouseButton(2))
+        {
+            this.transform.Rotate(0,0,5 * Input.GetAxis("Mouse X"));
+        }
+
+        if (Input.GetKeyDown("k"))
+        {
+            SnapFlag = true;
+            Debug.Log("save ");
+        }
+        else
+            SnapFlag = false;
+
+    }
+
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+        //mat is the material which contains the shader
+        //we are passing the destination RenderTexture to
+
+        if (SnapFlag)
+        {
+            RenderTexture currentRT = RenderTexture.active;   // save current active rendertexture
+            RenderTexture.active = destination;
+
+            Texture2D image = new Texture2D(destination.width, destination.height, TextureFormat.RGB24, false);
+            image.ReadPixels(new Rect(0, 0, destination.width, destination.height), 0, 0);
+            image.Apply();
+
+            savePNG(image, "D:/RFTracker/DepthImage/Assets/DepthImagesStore/bunnyCamera" + image_id + ".png");
+
+            image_id++;
+            RenderTexture.active = currentRT; // restore 
+        }
+    }
+    private void savePNG(Texture2D image, string path_file)
+    {
+        // store the texture into a .PNG file
+        byte[] bytes = image.EncodeToPNG();
+
+        // save the encoded image to a file
+        System.IO.File.WriteAllBytes(path_file, bytes);
+    }
+}
