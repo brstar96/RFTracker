158cca234a0294b73ce5afde960c020817c950d5
LEEMYEONGGYU <brstar96@naver.com>
2018-11-23 17:04:44 +0900

    BackProjection repair

--- DepthImage/Assets/CsScripts/BackProjection.cs
+++ DepthImage/Assets/CsScripts/BackProjection.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
 
-public class BackProjection : MonoBehaviour
+public class BackProjection_1122 : MonoBehaviour
 {
     string PATH = "D:/DepthTestImgs/0.png";
-    
+
     void Start()
     {
         BackProj();
@@ -15,14 +15,16 @@ public class BackProjection : MonoBehaviour
     {
         GameObject obj = GameObject.Find("empty");
 
-        List<GameObject> SphereList = new List<GameObject>(); //20개의 sphere 오브젝트를 만들기 위한 변수
-        CreateCube();
+        List<GameObject> SphereList = new List<GameObject>(); //20개의 sphere 오브젝트를 만들기 위한 리스트
+        Backprojection();
     }
 
-    private void CreateCube()
+    private void Backprojection()
     {
-        List<GameObject> SphereList = new List<GameObject>();
-        List<Vector3> vertexPositions = new List<Vector3>();
+        List<GameObject> SphereList = new List<GameObject>(); //게임씬에 Sphere를 생성하기 위한 리스트
+        List<Vector3> vertexPositions = new List<Vector3>(); //backprojection을 수행하기 위한 리스트
+        List<int> ShuffledVerts = new List<int>(20); //Nj(20개) vertices를 뽑기 위한 임시 리스트
+
         List<Vector3> grayScalePixcels = loadGrayScalePNG(PATH); //loading image from 'PATH' and store to 'testtex' Texture2D variable. 
         GameObject singleSphere;
         for (int j = 0; j < grayScalePixcels.Count; j++)
@@ -33,14 +35,14 @@ public class BackProjection : MonoBehaviour
         }
         for (int count = 0; count < grayScalePixcels.Count; count++)
         {
-            vertexPositions.Add(new Vector3(grayScalePixcels[count].x / 512f, grayScalePixcels[count].y / 512f, grayScalePixcels[count].z / 5.12f));
+            vertexPositions.Add(new Vector3(grayScalePixcels[count].x / 512f, grayScalePixcels[count].y / 512f, grayScalePixcels[count].z / 2.55f));
         }
 
         for (int j = 0; j < SphereList.Count; j++)
         {
             //SphereList[j].transform.position = new Vector3(vertexPositions[j].x, vertexPositions[j].y, vertexPositions[j].z);
-            SphereList[j].transform.position = new Vector3(vertexPositions[j].x, vertexPositions[j].y, vertexPositions[j].z * 5);
-           // getDisplacement(); //call 'getDisplacement' function for calculate displacement value per 'GT-backprojected vertex pair'. (11.18)
+            SphereList[j].transform.position = new Vector3(vertexPositions[j].x - 0.5f, vertexPositions[j].y - 0.5f, (vertexPositions[j].z * 2.5f) - 0.5f);
+            // getDisplacement(); //call 'getDisplacement' function for calculate displacement value per 'GT-backprojected vertex pair'. (11.18)
         }
 
     }
@@ -78,6 +80,22 @@ public class BackProjection : MonoBehaviour
         fileData = null;
     }
 
+    //public static void ListShuffle<T>(this IList<T> list)
+    //{
+    //    //Random random = new Random(); //Random클래스로부터 새 random 클래스 인스턴스(?) 생성
+    //    int n = list.Count; //받아온 list의 길이를 저장
+
+    //    for (int i = list.Count - 1; i > 1; i--)
+    //    {
+    //        int rnd = Random.Range(0, i + 1); //0부터 리스트길이만큼의 범위 사이에서 random int 생성
+
+    //        T value = list[rnd];
+    //        list[rnd] = list[i];
+    //        list[i] = value;
+
+    //    }
+    //}
+
     public void getDisplacement()
     {
         Vector3 a = new Vector3(2, 2, 2);
@@ -103,5 +121,4 @@ public class BackProjection : MonoBehaviour
 
     }
 }
-
 
