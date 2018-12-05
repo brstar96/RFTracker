1d7e167285e3ee7a13eaad1d36f7074c445ee527
LEEMYEONGGYU <brstar96@naver.com>
2018-12-05 13:20:14 +0900

    1204-003 Unity Major update
    1, Tracking stage(for Demo) update

--- Python Projects/RFTracker(Development)/src/RFLearning.py
+++ Python Projects/RFTracker(Development)/src/RFLearning.py
@@ -22,19 +22,9 @@ if(mode=="train"):
     features = pd.DataFrame(TrainData['feature'])
     targets = TrainData['target']
 
-    # 보통 입력 데이터(feature)를 X로, 레이블(또는 출력)을 y로 표시.
-    # train_test_split()는 list, numpy array, pandas 데이터프레임과 series 객체를 넣을 수 있음. (자동으로 무작위 표본을 선택해 split)
     X_train, X_test, y_train, y_test = train_test_split(features, targets, train_size=0.8, random_state=42)
-    # -----------------------------------------------------------------------------
-
-    # -------------------RF Estimator 학습수행-------------------------------------
-    # n_estimators는 포레스트에 존재하는 트리의 개수,
-    # oob_score는 보이지 않는('Unseen') 데이터로부터 R^2 Score를 추정하기 위해 OOB samples를 사용할지 여부,
-    # random_state는 난수 생성 프로그램에서 사용되는 시드(초기값).
     RF.fit(X_train, y_train)  # RF instance에 대해 X_train이라는 데이터를 받아와 y_train의 범주로 학습시킴.
-    # -----------------------------------------------------------------------------
 
-    # --------------------학습된 인스턴스 Export------------------------------------
     with open('RF.pkl', 'wb') as wb_pklhandler: #Pickle파일을 만들기 위한 쓰기 전용 파일핸들러 생성
         pickle.dump(RF, wb_pklhandler) #RF 인스턴스를 RF.pkl에 Dump
 
--- Unity Projects/RandomForestTraker1203/Assets/Script/TrackingScr.cs
+++ Unity Projects/RandomForestTraker1203/Assets/Script/TrackingScr.cs
@@ -1,16 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class TrackingScr : MonoBehaviour {
+    public Material mat;
+    private Camera cam;    
+    private GameObject GTPos, PredictedPos, DepthCamera;
+    string[] colums;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private string[] GTBunnyPositions, PredictedBunnyPositions;
+    StreamWriter writer;
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+        GTPos = GameObject.Find("GTPos");
+        PredictedPos = GameObject.Find("PredictedPos");
+        StartCoroutine("RunMove");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+     
+    }
+
+    //토끼를 이동하는 코루틴
+    IEnumerator RunMove()
+    {
+        //GTPos(X_test) csv파일을 읽어서 GTPos토끼의 포지션을 변경해줌. 
+        GTBunnyPositions = File.ReadAllLines("D:/RFTracker/Unity Projects/RandomForestTraker1203/Assets/DataFiles/gt.csv");
+        PredictedBunnyPositions = File.ReadAllLines("D:/RFTracker/Unity Projects/RandomForestTraker1203/Assets/DataFiles/predicted.csv");
+
+        for (int SimulationCount = 0; SimulationCount < 500; SimulationCount++)
+        {
+            //GTBunny의 Transformation을 변경하고 빨강색으로 렌더링. 
+            colums = GTBunnyPositions[SimulationCount].Split(',');
+            GTPos.transform.position = new Vector3(float.Parse(colums[0]) / 10, float.Parse(colums[1]) / 10, float.Parse(colums[2]) / 10);
+            GTPos.transform.localEulerAngles = new Vector3(float.Parse(colums[3]), float.Parse(colums[4]), float.Parse(colums[5]));
+            GTPos.GetComponent<MeshRenderer>().material.color = new Color((float)1.0f, (float)0.0f, (float)0.0f);
+            //yield return new WaitForSeconds(0.1f);
+
+            //PredictedBunny의 Transformation을 변경하고 파랑색으로 렌더링. 
+            colums = PredictedBunnyPositions[SimulationCount].Split(',');
+            PredictedPos.transform.position = new Vector3(float.Parse(colums[0]) / 10, float.Parse(colums[1]) / 10, float.Parse(colums[2]) / 10);
+            PredictedPos.transform.localEulerAngles = new Vector3(float.Parse(colums[3]), float.Parse(colums[4]), float.Parse(colums[5]));
+            PredictedPos.GetComponent<MeshRenderer>().material.color = new Color((float)0.0f, (float)0.0f, (float)1.0f);
+            yield return new WaitForSeconds(0.5f);
+
+            yield return null;
+        }
+    }
+
 }
